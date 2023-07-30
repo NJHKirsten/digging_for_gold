9e392810917790cbc435e8a8ea63f2091fd1bef2
Hannes Kirsten <hannes.kirsten@entelect.co.za>
2023-07-30 13:54:46 +0200

    changes

--- test_run.py
+++ test_run.py
@@ -2,7 +2,7 @@ from experiment_runner import ExperimentRunner
 
 
 def main():
-    ExperimentRunner.run(run_name="mlp_2_hidden_test_run_adam_2")
+    ExperimentRunner.run(run_name="mlp_2_hidden_test_run_adam")
 
 
 if __name__ == "__main__":
