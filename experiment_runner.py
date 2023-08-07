7a2b5617c12944e9a5e75dd352e42af5a96ef86f
Hannes Kirsten <hannes.kirsten@entelect.co.za>
2023-08-07 18:59:58 +0200

    changes

--- experiment_runner.py
+++ experiment_runner.py
@@ -1,9 +1,9 @@
 import os
-os.environ["OMP_NUM_THREADS"] = "10"
-os.environ["OPENBLAS_NUM_THREADS"] = "10"
-os.environ["MKL_NUM_THREADS"] = "10"
-os.environ["VECLIB_MAXIMUM_THREADS"] = "10"
-os.environ["NUMEXPR_NUM_THREADS"] = "10"
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["OPENBLAS_NUM_THREADS"] = "1"
+os.environ["MKL_NUM_THREADS"] = "1"
+os.environ["VECLIB_MAXIMUM_THREADS"] = "1"
+os.environ["NUMEXPR_NUM_THREADS"] = "1"
 import math
 import sys
 
